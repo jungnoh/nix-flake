abdff29dd7fe7b3cf7023d732ac14795a8db77a5
Junghoon Noh <junghoon.noh@devsisters.com>
2024-03-10 18:47:35 +0900

    Enable zsh

--- modules/cli/zsh.nix
+++ modules/cli/zsh.nix
@@ -1,8 +1,24 @@
 { config, lib, pkgs, ... }:
 {
-  home.programs.zsh = {
-    shellAliases = {
-      awslogin = "saml2aws login --force --session-duration=43200 --disable-keychain";
+  home.programs = {
+    thefuck = {
+      enable = true;
+    };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    zsh = {
+      enable = true;
+      dotDir = ".config/zsh";
+      shellAliases = {
+        awslogin = "saml2aws login --force --session-duration=43200 --disable-keychain";
+      };
+      oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = ["git" "kubectl" "dotenv"];
+      };
     };
   };
 }
