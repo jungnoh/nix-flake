9158d0330f217748db6097f56ae7dbd4626019de
Junghoon Noh <junghoon.noh@devsisters.com>
2024-03-14 22:33:52 +0900

    Don't use zplug, fix zshrc

--- modules/cli/zsh.nix
+++ modules/cli/zsh.nix
@@ -15,8 +15,11 @@
       export KUBE_EDITOR=vim
       export K9S_EDITOR=vim
       export EDITOR=vim
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-      (. $(dirname $(dirname $(readlink -f $(which asdf))))/asdf.sh) || (. $(dirname $(dirname $(readlink -f $(which asdf))))/share/asdf-vm/asdf.sh)
+      source ~/.p10k.zsh
+      '';
+      initExtra = ''
+      [[ ! -f $(dirname $(dirname $(readlink -f $(which asdf))))/asdf.sh ]] || source $(dirname $(dirname $(readlink -f $(which asdf))))/asdf.sh
+      [[ ! -f $(dirname $(dirname $(readlink -f $(which asdf))))/share/asdf-vm/asdf.sh ]] || source $(dirname $(dirname $(readlink -f $(which asdf))))/share/asdf-vm/asdf.sh
       '';
       shellAliases = {
         awslogin = "saml2aws login --force --session-duration=43200 --disable-keychain";
@@ -29,13 +32,13 @@
         k = "kubectl";
         kg = "kubectl get";
       };
-      zplug = {
-        enable = true;
-        plugins = [
-          { name = "bigH/git-fuzzy"; tags = ["as:command" "use:\"bin/git-fuzzy\""]; }
-          { name = "romkatv/powerlevel10k"; tags = ["as:theme" "depth:1"]; }
-        ];
-      };
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+      ];
     };
   };
   home.file.".p10k.zsh".text = (builtins.readFile ./p10k.zsh);
