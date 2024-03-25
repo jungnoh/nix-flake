b18f736c61b1c1cbc42ca874b749d79c03f00af1
Junghoon Noh <junghoon.noh@devsisters.com>
2024-03-25 21:44:04 +0900

    Add system.default for finder and dock

--- flake.nix
+++ flake.nix
@@ -33,6 +33,20 @@
       system.stateVersion = 4;
 
       security.pam.enableSudoTouchIdAuth = true;
+      system.defaults = {
+        finder = {
+          _FXShowPosixPathInTitle = true;  # show full path in finder title
+          AppleShowAllExtensions = true;  # show all file extensions
+          FXEnableExtensionChangeWarning = false;  # disable warning when changing file extension
+          QuitMenuItem = true;  # enable quit menu item
+          ShowPathbar = true;  # show path bar
+          ShowStatusBar = true;  # show status bar
+        };
+        dock = {
+          autohide = true;
+          show-recents = false;  # disable recent apps
+        };
+      };
     };
     system = "aarch64-darwin";
     
