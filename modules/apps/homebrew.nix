55e71f6ef6d024e0951539903423edd6029716a4
Junghoon Noh <jungnoh.dev@gmail.com>
2025-12-10 17:59:08 +0900

    Update used packages

--- modules/apps/homebrew.nix
+++ modules/apps/homebrew.nix
@@ -14,6 +14,7 @@
       "telegram"
       "spotify"
       "obsidian"
+      "claude"
       # Dev tools
       "cursor"
       "insomnia"
--- modules/apps/jetbrains.nix
+++ modules/apps/jetbrains.nix
@@ -2,10 +2,7 @@
 {
   config = {
     home.packages = with pkgs.unstable; [
-      jetbrains.datagrip
-      jetbrains.goland
-      jetbrains.idea-ultimate
-      jetbrains.rider
+      jetbrains-toolbox
     ];
   };
 }
