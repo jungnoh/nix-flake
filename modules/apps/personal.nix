bf5071b7464f02717e6fadb28a587b924a56d86b
Junghoon Noh <jungnoh.dev@gmail.com>
2026-01-10 20:09:28 +0900

    Merge

--- modules/apps/personal.nix
+++ modules/apps/personal.nix
@@ -7,10 +7,14 @@
       font-awesome
       rclone
     ];
+    homebrew.brews = [
+      "gemini-cli"
+    ];
     homebrew.casks = [
       "tailscale-app"
       "losslesscut"
       "mullvad-vpn"
+      "claude-code"
     ];
     home.programs.zsh.shellAliases = {
       tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
