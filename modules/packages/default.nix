bdb1d00b0086f6305f3e4ea9559b95be1c3b17e0
Junghoon Noh <jungnoh.dev@gmail.com>
2026-01-16 16:29:34 +0900

    Rename folders

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -11,25 +11,20 @@ let
     );
 
   commonModules =
-    (
-      if isDarwin then
-        (listNixFiles ./01-system/darwin) ++ (listNixFiles ./02-app-darwin)
-      else
-        (listNixFiles ./01-system/linux)
-    )
+    (if isDarwin then (listNixFiles ./01-system/darwin) else (listNixFiles ./01-system/linux))
     ++ (listNixFiles ./02-app-common);
 
   featuresModuleMap = {
-    personal = [ ./02-app-envs/personal.nix ];
-    work = [ ./02-app-envs/work.nix ];
     desktop = [
-      ./02-app-envs/desktop.nix
-      ./02-app-envs/desktop-basic.nix
+      ./02-profiles/desktop.nix
+      ./02-profiles/desktop-basic.nix
     ];
     dev-env = [
-      ./02-app-envs/dev-env.nix
-      ./02-app-envs/dev-env-cloud.nix
+      ./02-profiles/dev-env.nix
+      ./02-profiles/dev-env-cloud.nix
     ];
+    personal = [ ./02-profiles/personal.nix ];
+    work = [ ./02-profiles/work.nix ];
   };
 
   featureModules = builtins.concatMap (p: featuresModuleMap."${p}") features;
