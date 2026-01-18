00c7810f59d34770e09af04eb6fe23fae37d22d7
Junghoon Noh <jungnoh.dev@gmail.com>
2026-01-18 22:52:16 +0900

    linux: Add xclip

--- modules/packages/01-system/linux/common.nix
+++ modules/packages/01-system/linux/common.nix
@@ -10,5 +10,14 @@
       usbutils
       nvme-cli
     ];
+
+    home.packages = with pkgs.unstable; [
+      xclip
+    ];
+
+    home.shellAliases = {
+      pbcopy = "xclip -selection clipboard";
+      pbpaste = "xclip -selection clipboard -o";
+    };
   };
 }
