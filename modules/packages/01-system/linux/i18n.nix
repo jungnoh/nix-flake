a5b45bb27934e20a0c2eb72f1036876241f75f1a
Junghoon Noh <jungnoh.dev@gmail.com>
2026-01-30 20:40:05 +0900

    Use fcitx5 for i18n.inputMethod

--- modules/packages/01-system/linux/i18n.nix
+++ modules/packages/01-system/linux/i18n.nix
@@ -22,9 +22,41 @@ in
   # https://github.com/Riey/kime
   i18n.inputMethod = {
     enable = true;
-    type = "kime";
-    kime.iconColor = "White";
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        fcitx5-hangul
+      ];
+      settings = {
+        inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "hangul";
+          };
+          "Groups/0/Items/0" = {
+            Name = "keyboard-us";
+            Layout = "";
+          };
+          "Groups/0/Items/1" = {
+            Name = "hangul";
+          };
+          "Groups/0/Items/2" = {
+            Name = "mozc";
+          };
+          GroupOrder = {
+            "0" = "Default";
+          };
+        };
+      };
+    };
   };
+  home.packages = with pkgs; [
+    kdePackages.fcitx5-configtool
+  ];
 
   fonts.enableDefaultPackages = false;
   fonts.packages = with pkgs; [
