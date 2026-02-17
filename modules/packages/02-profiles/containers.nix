88b83eecb01413c8e20dc7df02b3c68281827db5
Junghoon Noh <jungnoh.dev@gmail.com>
2026-02-17 13:40:59 +0900

    Fix container related packages config

--- modules/packages/02-profiles/containers.nix
+++ modules/packages/02-profiles/containers.nix
@@ -4,18 +4,33 @@
   config,
   lib,
   pkgs,
+  ctx,
   ...
 }:
+let
+  inherit (ctx) onlyDarwin onlyLinux;
+in
 {
-  home.packages = with pkgs.unstable; [
-    colima
-    lima
-    docker
-    docker-compose
-    virtio-win
+  config = lib.mkMerge [
+    {
+      home.packages = with pkgs.unstable; [
+        colima
+        lima
+        docker
+        docker-compose
+      ];
+    }
+    (onlyDarwin {
+      homebrew.casks = [
+        "utm"
+      ];
+    })
+    (onlyLinux {
+      virtualisation.libvirtd.enable = true;
+      programs.virt-manager.enable = true;
+      home.packages = with pkgs.unstable; [
+        virtio-win
+      ];
+    })
   ];
-
-  # virt-manager
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
 }
