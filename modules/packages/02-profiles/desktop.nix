e0d73211387d329bdcd9931ae6f59d25711e789e
Junghoon Noh <jungnoh.dev@gmail.com>
2026-02-27 18:02:03 +0900

    Add some packages

--- modules/packages/02-profiles/desktop.nix
+++ modules/packages/02-profiles/desktop.nix
@@ -35,6 +35,7 @@ in
         spotify
         obsidian
         remmina
+        mpv 
         _1password-gui
       ];
     })
--- modules/packages/02-profiles/personal.nix
+++ modules/packages/02-profiles/personal.nix
@@ -36,6 +36,20 @@ in
         discord
         slack
       ];
+      services.mullvad-vpn = {
+        enable = true;
+        package = pkgs.unstable.mullvad-vpn;
+      };
+      services.resolved = {
+        enable = true;
+        dnssec = "true";
+        domains = [ "~." ];
+        fallbackDns = [
+          "1.1.1.1#one.one.one.one"
+          "1.0.0.1#one.one.one.one"
+        ];
+        dnsovertls = "true";
+      };
     })
   ];
 }
