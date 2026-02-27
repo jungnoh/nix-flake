def9c80f32705663c15bb0634d27df3aff0e393e
Junghoon Noh <jungnoh.dev@gmail.com>
2026-02-27 14:50:10 +0900

    Add zed

--- modules/packages/02-profiles/dev-env.nix
+++ modules/packages/02-profiles/dev-env.nix
@@ -21,6 +21,41 @@ in
         opencode
         claude-code
       ];
+      home.programs.zed-editor = {
+        enable = true;
+        extensions = [
+          "nix"
+          "toml"
+          "rust"
+          "html"
+          "dockerfile"
+          "sql"
+          "make"
+          "material-icon-theme"
+          "scss"
+          "csharp"
+          "terraform"
+          "opencode"
+          "csv"
+          "proto"
+          "just"
+        ];
+        userSettings = {
+          icon_theme = "Material Icon Theme";
+          theme = {
+            mode = "dark";
+            dark = "Ayu Dark";
+            light = "Ayu Light";
+          };
+          hour_format = "hour24";
+
+          base_keymap = "VSCode";
+          vim_mode = true;
+
+          # Tell Zed to use direnv and direnv can use a flake.nix environment
+          load_direnv = "shell_hook";
+        };
+      };
     }
     (onlyDarwin {
       homebrew.casks = [
