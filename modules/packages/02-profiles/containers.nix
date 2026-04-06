c4c102d425a0d79f557a599e0cd4f5477c7b6073
Junghoon Noh <jungnoh.dev@gmail.com>
2026-04-06 23:53:09 +0900

    Use podman in Linux

--- modules/packages/02-profiles/containers.nix
+++ modules/packages/02-profiles/containers.nix
@@ -11,11 +11,25 @@ in
   config = lib.mkMerge [
     {
       home.packages = with pkgs; [
-        colima
-        lima
         docker
         docker-compose
       ];
     }
+    (onlyDarwin {
+      home.packages = with pkgs; [
+        colima
+        lima
+      ];
+    })
+    (onlyLinux {
+      virtualisation = {
+        containers.enable = true;
+        podman = {
+          enable = true;
+          dockerCompat = true;
+          defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+        };
+      };
+    })
   ];
 }
