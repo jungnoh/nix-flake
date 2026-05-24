1e21a0e3f857add166e18b1006ca4b5b9ddd2c60
Junghoon Noh <jungnoh.dev@gmail.com>
2026-05-24 13:22:45 +0900

    tomori: Add pacakges for ripping

--- devices/tomori/configuration.nix
+++ devices/tomori/configuration.nix
@@ -4,20 +4,16 @@
 
 {
   ctx,
+  pkgs,
   ...
 }:
 let
   inherit (ctx) username;
 in
 {
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -37,16 +33,7 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${username} = {
@@ -58,21 +45,13 @@ in
     ];
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Links
+  # MakeMKV beta key: https://forum.makemkv.com/forum/viewtopic.php?t=1053
+  # AACS keys at FindVUK: https://fvonline-db.bplaced.net/
+  home.packages = with pkgs; [
+    abcde
+    makemkv
+  ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
